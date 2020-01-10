6dca7de4dda391070023a309e3ea9f666cfacb68
andemarco <andreademarco91@gmail.com>
2020-01-10 17:15:28 +0100

    UPLOAD ESERCIZI CLASSE

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -3,8 +3,12 @@ var numeroDue = parseInt(prompt('Inserisci qui il tuo secondo numero'));
 
 if (numeroUno > numeroDue) {
   console.log(numeroUno);
+  var winner = numeroUno
 } else if (numeroUno < numeroDue) {
   console.log(numeroDue);
+  var winner = numeroDue
 } else {
   console.log('uguali');
 }
+
+document.getElementById('winner').innerHTML = 'Il numero più grande inserito è ' + winner
